6ab825d4827a8b911cf2f1c639f52f263155a06a
bhishma620 <111000117+bhishma620@users.noreply.github.com>
2022-08-29 22:45:21 +0530

    Create Newline.c
    Printing Statements in newline in output console.

---
+++ C Programming/Fundamentals/Newline.c
@@ -0,0 +1,9 @@
+// In this program we will print the statements in multiple lines
+
+
+#include<stdio.h>
+int main(){
+  //using '\n' we can print statements in a newline 
+  printf("I am a Devoloper.\nNow i am a student.");
+  return 0;
+}
