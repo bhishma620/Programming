425f1a430176527af85850db34c71bd0d8799774
bhishma620 <111000117+bhishma620@users.noreply.github.com>
2022-08-29 23:05:00 +0530

    Update BubbleSort.c
    BUBBLE SORT

--- Data Structure/Array/BubbleSort.c
+++ Data Structure/Array/BubbleSort.c
@@ -1,1 +1,33 @@
+//SORTING ARRAY IN ASSENDING ORDER
+#include<stdio.h>
+int main (){
+    int arr[5],n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+     printf("Enter the element :");
+    for (int i = 0; i <n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-1-i; j++)
+        {
+        
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
 
+        }
+    }
+    printf("After sorting elements are:");
+    for (int i = 0; i <n; i++)
+    {
+   printf("%d ",arr[i]);
+    }
+    
+    
+    
+}
